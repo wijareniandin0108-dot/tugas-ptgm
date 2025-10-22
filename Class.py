67948c7a7be8ea8f437a7ef3bb00c02a8cca1016
wijareniandin0108-dot <wijareniandin0108@gmail.com>
2025-10-22 14:34:22 +0700

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,41 @@
+class Sekolah :
+  pass
+  
+class Siswa :
+    pass 
+    
+    
+class Sekolah :
+  pass
+      
+sekolah1 = Sekolah()
+sekolah2 = Sekolah()
+
+print(type(sekolah1))
+print(type(sekolah2))
+
+
+class Siswa :
+  pass
+ 
+siswa1 = Siswa()
+siswa2 = Siswa()
+
+print(type(siswa1))
+print(type(siswa2))
+
+
+class Sekolah :
+  pass 
+
+sekolah1 = Sekolah()
+sekolah1.nama = "SMK PGRI 35 Solokan Jeruk"
+sekolah1.alamat = "JL RO SEOKARNO no 03 sakbengkung"
+sekolah2 = Sekolah()
+sekolah2.nama = "SMK 9 BANDUNG"
+sekolah2.alamat = "JL RO MAWAR no 04 Bandung"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
